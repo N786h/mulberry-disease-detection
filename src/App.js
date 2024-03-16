307e22c9139c9aed12e014428040f0e3e002f0e4
N786h <nazeerhaider4@gmail.com>
2024-03-17 01:50:06 +0530

    proxy

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
     const payload = {
       message: userInput,
     };
-    fetch('https://mulberry-backend.onrender.com/chat', {
+    fetch('http://127.0.0.1:5000/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify( payload ),
@@ -67,7 +67,7 @@ function App() {
     const formData = new FormData();
     formData.append('file', blob);
 
-    fetch('https://mulberry-backend.onrender.com/upload', {
+    fetch('http://127.0.0.1:5000/upload', {
       method: 'POST',
       body: formData,
     })
