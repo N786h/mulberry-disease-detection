a199db76ea356aba5784d4e7654f551cd60c4bbe
N786h <nazeerhaider4@gmail.com>
2024-05-08 18:30:32 +0530

    Changed backedn address

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
     const payload = {
       message: userInput,
     };
-    fetch('http://127.0.0.1:5000/chat', {
+    fetch('https://bug-free-orbit-g5rv7v6xp943pgvx-5000.app.github.dev/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify( payload ),
@@ -67,7 +67,7 @@ function App() {
     const formData = new FormData();
     formData.append('file', blob);
 
-    fetch('http://127.0.0.1:5000/upload', {
+    fetch('https://bug-free-orbit-g5rv7v6xp943pgvx-5000.app.github.dev/upload', {
       method: 'POST',
       body: formData,
     })
